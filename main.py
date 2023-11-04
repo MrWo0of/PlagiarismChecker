ed7cf957acc7123614963f0377e622942ba1af7d
Isaac_Goff <goffi@lopers.unk.edu>
2023-11-03 22:53:03 -0500

    Uncommented code is from GeeksForGeeks, just here for learning, do not mistake for my own work

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from difflib import SequenceMatcher
 
 file = [f for f in os.listdir() if f.endswith('.txt')]
 print(file)
+
+# Read the content of each student's file
+student_docs = [open(f).read() for f in file]
+
+# Print the list of student files and their content
+for filename, document in zip(file, student_docs):
+    print(f"File: {filename}")
+    print("Content:")
+    print(document)
+    print("-" * 30)  # Separator between documents
 '''
 testFile = open('Files/Test1.txt', 'r')
 testFile2 = open('Files/Test2.txt', 'r')
