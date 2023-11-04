3be91b0d3222ed58a3c08e8f885d9f2ad5e2cec1
Isaac_Goff <goffi@lopers.unk.edu>
2023-11-03 21:47:46 -0500

    A start, getting Git started

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from difflib import SequenceMatcher
 import Files
+
+
+testFile = open('Files/Test1', 'r')
+testFile2 = open('Files/Test2', 'r')
+
+file1 = testFile.read()
+file2 = testFile.read()
+
+ab = SequenceMatcher(None, file1, file2).ratio()
+
+print(int(ab * 100))
