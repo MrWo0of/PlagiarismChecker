ada9dfdb5676ca8e90df92c71fb416658c7547d5
Isaac_Goff <goffi@lopers.unk.edu>
2023-11-05 00:46:56 -0500

    Added a handful of files from IvyPanda.com as test files for checking, test files to test percentage amounts.  Small error fixes, change to when 'plagiarism not present in specified amount' message shows

--- DataStructure.py
+++ DataStructure.py
@@ -1,10 +1,11 @@
 from collections.abc import MutableMapping
 
-# Homebrew class created from MutableMapping base.
-# MOSTLY FROM
-class HomebrewMap(MutableMapping):
+# Mapper class created from MutableMapping base.
+# MOSTLY FROM 'DATA STRUCTURES AND ALGORITHMS IN PYTHON' by Goodrich, et al.
+class Mapper(MutableMapping):
     __slots__ = '_key', '_value', '_table'
 
+    # Nested class used to store key-value pairs, do comparisons
     class Item:
         def __init__(self, key, value):
             self._key = key
@@ -19,15 +20,27 @@ class HomebrewMap(MutableMapping):
         def __lt__(self, other):
             return self._key < other._key
 
+        def __gt__(self, other):
+            return self._key > other._key
+
+        def __le__(self, other):
+            return self._key <= other._key
+
+        def __ge__(self, other):
+            return self._key >= other._key
+
+    # Map initialized with empty list for table
     def __init__(self):
         self._table = []
 
+    # Get item by checking if key in table and returning item
     def __getitem__(self, k):
         for item in self._table:
             if k == item._key:
                 return item._value
         raise KeyError('Key not in dict')
 
+    # Set item for key or change value of key if key exists
     def __setitem__(self, k, v):
         for item in self._table:
             if k == item._key:
@@ -35,6 +48,7 @@ class HomebrewMap(MutableMapping):
                 return
         self._table.append(self.Item(k, v))
 
+    # Search for item then remove key-value pair from table
     def __delitem__(self, k):
         for j in range(len(self._table)):
             if k == self._table[j]._key:
@@ -42,13 +56,16 @@ class HomebrewMap(MutableMapping):
                 return
         raise KeyError('Key does not exist')
 
+    # Give length of map
     def __len__(self):
         return len(self._table)
 
+    # Allow for iteration over map
     def __iter__(self):
         for item in self._table:
             yield item._key
 
+    # Comparison functions
     def __lt__(self, other):
         return len(self) < len(other)
 
@@ -62,15 +79,19 @@ class HomebrewMap(MutableMapping):
         return len(self) >= len(other)
 
 
+# Class created for file-splitting functions
 class FileSplitter:
-    def __init__(self):
-        percent = 0
 
+    #Separate function created to accept file from user
     def separate(self, file):
-        lineMap = HomebrewMap()
+        # Map storing lines and line number created, file split by sentences
+        lineMap = Mapper()
         lineNum = 1
         openedFile = open(file, 'r', encoding='mbcs')
         lines = openedFile.read().split('.')
+
+        # For loop strips lines of newline characters, organizes words
+        # in sentences alphabetically and reappends to map
         for line in lines:
             currentLine = line.strip('\n')
             sepLine = currentLine.split(' ')
@@ -79,21 +100,32 @@ class FileSplitter:
             lineNum += 1
         return lineMap
 
+    # Function for comparing two maps created
     def compare(self, dict1, dict2):
+
+        # Variable created.  Will be accumulated per exact match of words in
+        # line then divided by length of smaller file to find total percent of same lines
         score = 0
-        size = 1
+
+        # Check if dict1 is larger than dict2, then use dict2 as base file for checks
         if len(dict1) >= len(dict2):
             size = len(dict2)
             for i in range(len(dict2)):
                 if dict2[i + 1] in dict1.values() and dict2[i + 1] != '':
                     score += 1
+
+        # Elif dict2 is larger, use dict1 for checks instead
         elif dict1 < dict2:
             size = len(dict1)
             for i in range(len(dict1)):
                 size = len(dict1)
                 if dict1[i + 1] in dict2.values() and dict1[i + 1] != '':
                     score += 1
+
+        # Catch for empty files being used
         else:
             raise IndexError('One of the maps is empty')
+
+        # Percent calculated then returned to user
         total = round(score / size, 2)
         return total
--- main.py
+++ main.py
@@ -1,69 +1,100 @@
+#AUTHOR - Isaac Goff
+# DATE - 11/04/2023
+# ALGORITHM -
+# STEP 1 - Ask user for mode to check percentage plagiarized and file to check
+# STEP 2 - Strip and separate input file into map, do same for all files in Files
+# STEP 3 - Using maps, find percent of lines with same wording between input file
+#          and stored files based on mode, display to user
+# STEP 4 - Ask user if they wish to append file into Files folder,
+#          ask for rerun or end
+
+# os imported for accessing and moving files outside of directory
 import os
+
+# DataStructure imported for splitting, mapping, and comparing of files
 import DataStructure
 
+# Sentinel, DataStructure variables made
 finished = False
 filesep = DataStructure.FileSplitter()
+
+# While loop checks if user has ended program after cycle
 while finished is not True:
-    modeDict = {'p': 'Percent Mode', 'q': 'Quarter Mode', 'h': 'Half Mode',
-                't': 'Three-Fourths mode', 'f': 'Fully Plagiarised mode'}
+
+    # User input determines what files will be marked by system, gets file to check
     mode = input('Enter "p" for total percent of content plagiarized from all files\n'
                  'Enter "q" to check for files with above 25% plagiarized\n'
                  'Enter "h" to check for files with above 50% plagiarized\n'
                  'Enter "t" to check for files with above 75% plagiarized\n'
                  'Enter "f" to check for 100% plagiarism in files\n>    ')
-
     checkFile = input('Enter name of file to check: ')
+
+    # Input file stripped of tail for name variable, separated for map variable
     checkFileName = checkFile.strip('.txt')
     checkFileRead = filesep.separate(checkFile)
 
-    # Directory variable.
+    # Directory variable as all files stored within project
     direc = 'Files/'
+
+    # Lists made for file contents and names respectively
     fileList = []
     fileNames = []
+
+    # For file in Files directory, append name, separate and add to list
     for file in os.listdir('Files/'):
         fileNames.append(file)
         filepath = direc + file
         convertedFile = filesep.separate(filepath)
         fileList.append(convertedFile)
 
-    for file in fileList:
-        documents = file.values()
+    checkNoPlagiarism = 0
 
+    # For every file and name
     for dict1,fileNames in zip(fileList, fileNames):
+
+        # Try comparing files, check mode to see what files should be marked
         try:
             dict2 = checkFileRead
             result = round(filesep.compare(dict1, dict2) * 100)
             if mode == 'p':
                 print(f'{result}% potential plagiarism detected in {checkFileName} matching with {fileNames}')
+                checkNoPlagiarism = 1
             elif result >= 25 and mode == 'q':
                 print(f'Potential plagiarism detected in {checkFileName} matching {result}% with {fileNames}')
+                checkNoPlagiarism = 1
             elif result >= 50 and mode == 'h':
                 print(f'Potential plagiarism detected in {checkFileName} matching {result}% with {fileNames}')
+                checkNoPlagiarism = 1
             elif result >= 75 and mode == 't':
                 print(f'Potential plagiarism detected in {checkFileName} matching {result}% with {fileNames}')
+                checkNoPlagiarism = 1
             elif result >= 100 and mode == 'f':
                 print(f'Potential plagiarism detected in {checkFileName} matching {result}% with {fileNames}')
-            else:
-                print(f'Plagiarism not within amount specified for {fileNames}')
+                checkNoPlagiarism = 1
+
+        # Except Index Error (Incase file empty) continue past error
         except IndexError:
             continue
 
+    if checkNoPlagiarism == 0:
+        print(f'Plagiarism not within amount specified for '
+              f'{checkFileName} compared to files in database')
 
-
-
+    # Ask if user wants to append file to Files directory for future checking
     addfile = input('\nAdd file to files directory for future checking? y/n: ')
+
+    # If yes, add file into directory and tell user, else tell user otherwise
     if addfile.lower() == 'y':
         os.replace(str(checkFile), direc + str(checkFile))
         print(f"{checkFileName} added to Files directory")
     else:
         print(f"{checkFileName} not added")
 
+    # Ask user if they are done, if y, end while loop
     end = input('Finished? y/n:')
     if end.lower() == 'y':
         finished = True
     print()
 
-
-
-
+# Ending statement
 print('Thank you for using the Plagiarism Checker\nHave a good day')
